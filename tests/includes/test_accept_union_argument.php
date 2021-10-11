3e02652f0816345fc9c0aee69869efea6e99f7d8
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-10-11 10:59:08 +0900

    Fix and Add tests

--- tests/includes/test_accept_union_argument.php
+++ tests/includes/test_accept_union_argument.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Mpyw\EloquentHasByNonDependentSubquery\Tests\Models\Comment;
 use Mpyw\EloquentHasByNonDependentSubquery\Tests\Models\Post;
@@ -24,6 +25,59 @@ EOD
     Comment::query()->hasByNonDependentSubquery(
         'post',
         function (Post|BelongsTo $query) {
+            $this->assertInstanceof(BelongsTo::class, $query);
+            $query->onlyTrashed();
+        }
+    )->withTrashed()
+);
+
+$this->assertQueryEquals(
+    <<<'EOD'
+            select
+                *
+            from
+                "comments"
+            where
+                "comments"."post_id" in (
+                    select
+                        "posts"."id"
+                    from
+                        "posts"
+                    where
+                        "posts"."deleted_at" is not null
+                )
+EOD
+    ,
+    Comment::query()->hasByNonDependentSubquery(
+        'post',
+        function (BelongsTo|Builder $query) {
+            $this->assertInstanceof(BelongsTo::class, $query);
+            $query->onlyTrashed();
+        }
+    )->withTrashed()
+);
+
+$this->assertQueryEquals(
+    <<<'EOD'
+            select
+                *
+            from
+                "comments"
+            where
+                "comments"."post_id" in (
+                    select
+                        "posts"."id"
+                    from
+                        "posts"
+                    where
+                        "posts"."deleted_at" is not null
+                )
+EOD
+    ,
+    Comment::query()->hasByNonDependentSubquery(
+        'post',
+        function (Post|Builder $query) {
+            $this->assertInstanceof(Builder::class, $query);
             $query->onlyTrashed();
         }
     )->withTrashed()
